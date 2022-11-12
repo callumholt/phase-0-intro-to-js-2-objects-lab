99dc71597d46b32452b564bea60a83ddb8eeb0df
callum <callumpholt@gmail.com>
2022-11-12 11:35:28 +1100

    added function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution in this file!
+const employee = {
+    name: "john",
+    streetAddress: "123 abc street",
+};
+
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+    
+}
